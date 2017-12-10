0e673db23953f521cafb0d1fd115c093c8532dbd
lsmartins <leonardo.martins@neppo.com.br>
2017-12-10 20:02:20 -0200

    Add templates with Jinja2

--- graph.py
+++ graph.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import json
 import pprint as p
 from networkx.readwrite import json_graph
+import os
+from jinja2 import Environment, FileSystemLoader
 
 def load(fileName):
 	with open(fileName+'.json') as json_file: 
@@ -17,27 +19,38 @@ def save(topology):
 
 
 def createFlow(F, source, destiny, flow, flows, id):
-	currentSourceCapacity = F.node[source]['demand']
-	currentDestinyCapacity = F.node[destiny]['demand']
-	F.node[source]['demand'] = currentSourceCapacity+(-flow)
-	F.node[destiny]['demand'] = currentDestinyCapacity+(flow)
-	flowCost, flowDict = nx.capacity_scaling(F, capacity='current_capacity')
-	print(flowDict)
-	for i in flowDict:
-		for j in flowDict[i]:
-			currentCapacity = F.edge[i][j]['current_capacity']
-			F.edge[i][j]['current_capacity'] =  currentCapacity-flowDict[i][j]
-	flows[id] = flowDict		   
-	F.node[source]['demand'] = 0
-	F.node[destiny]['demand'] = 0
-	return F, flows
+        env = Environment(loader=FileSystemLoader(os.path.join('.', 'templates')))
+        template = env.get_template('commands-template.txt')
+        arq1 = open("util/s1_commands.txt", "w")
+        arq2 = open("util/s5_commands.txt", "w")
+        currentSourceCapacity = F.node[source]['demand']
+        currentDestinyCapacity = F.node[destiny]['demand']
+        F.node[source]['demand'] = currentSourceCapacity+(-flow)
+        F.node[destiny]['demand'] = currentDestinyCapacity+(flow)
+        flowCost, flowDict = nx.capacity_scaling(F, capacity='current_capacity')
+        for i in flowDict:
+                for j in flowDict[i]:
+                        currentCapacityij = F.edge[i][j]['current_capacity']
+                        currentCapacityji = F.edge[j][i]['current_capacity']
+                        F.edge[i][j]['current_capacity'] = currentCapacityij-flowDict[i][j]
+                        F.edge[j][i]['current_capacity'] = currentCapacityji-flowDict[i][j]
+        arq1.write(template.render(source_ip=F.node[source]['ip'], destination_ip=F.node[destiny]['ip'], first=format(F.edge[2][3]['capacity']-F.edge[2][3]['current_capacity'], '08b'), second=format(F.edge[2][4]['capacity']-F.edge[2][4]['current_capacity'], '08b'), third=format(F.edge[2][5]['capacity']-F.edge[2][5]['current_capacity'], '08b')))
+        arq2.write(template.render(source_ip=F.node[destiny]['ip'], destination_ip=F.node[source]['ip'], first=format(F.edge[2][3]['capacity']-F.edge[2][3]['current_capacity'], '08b'), second=format(F.edge[2][4]['capacity']-F.edge[2][4]['current_capacity'], '08b'), third=format(F.edge[2][5]['capacity']-F.edge[2][5]['current_capacity'], '08b')))
+        flows[id] = flowDict		   
+        F.node[source]['demand'] = 0
+        F.node[destiny]['demand'] = 0
+        arq1.close()
+        arq2.close()
+        return F, flows
 
 def destroyFlow(F, flows, id):
 	if id in flows:
 		for i in flows[id]:
 			for j in flows[id][i]:
-				currentCapacity = F.edge[i][j]['current_capacity']
-				F.edge[i][j]['current_capacity'] =  currentCapacity+flows[id][i][j]
+                                currentCapacityij = F.edge[i][j]['current_capacity']
+                                currentCapacityji = F.edge[i][j]['current_capacity']
+                                F.edge[i][j]['current_capacity'] = currentCapacityij+flows[id][i][j]
+                                F.edge[j][i]['current_capacity'] = currentCapacityji+flows[id][i][j]
 		del flows[id]		
 	else:
 		print("Invalid id")
@@ -47,27 +60,6 @@ def listId(flows):
 	for key, value in flows.items():
 		print("ID: ", key, " - Value : ", value)
 
-# def add(F,):
-# 	for i in flowDict:
-# 		for j in flowDict[i]:
-# 			if not bool(F.node[i]['teste']):
-
-# 			print(F.node[i]['teste'][i][j])
-
-# G=nx.DiGraph()
-
-# G.add_node(0, id=0, teste={}, demand=0, ip='127.0.0.0')
-# G.add_node(1, id=1, teste={}, demand=0, ip='127.0.0.1')
-# G.add_node(2, id=2, teste={}, demand=0, ip='127.0.0.2')
-# G.add_node(3, id=3, teste={}, demand=0, ip='127.0.0.3')
-
-# G.add_edge(0,1, capacity=10, current_capacity=10, weight=1)
-# G.add_edge(0,3, capacity=9, current_capacity=9, weight=1)
-# G.add_edge(0,2, capacity=8, current_capacity=8, weight=1)
-# G.add_edge(1,2, capacity=7, current_capacity=7, weight=1)
-# G.add_edge(2,3, capacity=6, current_capacity=6, weight=1)
-
-# save(G)
 F = load("topology")
 count = 0
 flows = {}
@@ -75,26 +67,19 @@ while True:
     try:
         option=int(input('1 - Create Flow  2 - Delete Flow  3 - List Valid Ids  4 - Draw  5 - Exit: '))
         if option == 1:
-        	print("---------- CREATING FLOW ----------")
-        	source=int(input('Insert source: '))
-        	destiny=int(input('Insert destiny: '))
-        	flow=int(input('Insert flow: '))
-        	F, flows = createFlow(F,source,destiny,flow, flows, count)
-        	print(flows)
-        	count=count+1
-        	print(F.edge[0][2])
-        	print(F.edge[0][1])
-        	print(F.edge[1][2])
-        	#(G.edge[1]['teste'])[1]=2
-        	# print(G.edges())
-        	# print(G.edge[1][teste][1])            
+                print("---------- CREATING FLOW ----------")
+                source=int(input('Insert source: '))
+                destiny=int(input('Insert destiny: '))
+                flow=int(input('Insert flow: '))
+                F, flows = createFlow(F,source,destiny,flow, flows, count)
+        	    count=count+1
+                os.system("python tools/apply_commands.py")            
         if option == 2:
-        	print("---------- DESTROYING FLOW ----------")
-        	id=int(input('Insert id: '))
-        	F, flows = destroyFlow(F,flows, id)
-        	print(F.edge[0][2])
-        	print(F.edge[0][1])
-        	print(F.edge[1][2])
+                print("---------- DESTROYING FLOW ----------")
+                id=int(input('Insert id: '))
+                arq = open("commands.txt", "w")
+                F, flows = destroyFlow(F,flows, id)
+                arq.close()
         if option == 3:
         	print("---------- LIST ----------")
         	listId(flows)
@@ -119,7 +104,3 @@ while True:
         	break	    
     except ValueError:
         print("Not a number")
-
-
-# nx.draw(F)
-# plt.show()
